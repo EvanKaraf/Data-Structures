af7bb81c2812ec5cb868240a411fb1f480975c9d
EvanKaraf <vaggeliskaraf@gmail.com>
2016-06-01 19:09:00 +0300

    Delete EverythingATM.h

--- MazeProblemDFS/EverythingATM.h
+++
@@ -1,4 +0,0 @@
-int ** initializeBoard(int *rows,int *columns);
-int** createVisited(int rows,int columns);
-void resetVisited(int **visited,int rows,int columns);
-void freeVisited(int **visited,int rows);
