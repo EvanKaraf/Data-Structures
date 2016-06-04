88560f27eff520dde73a274e37258bd80005f02a
EvanKaraf <vaggeliskaraf@gmail.com>
2016-06-04 06:08:52 +0300

    Update menu.c

--- Linear_Hashing/menu.c
+++ Linear_Hashing/menu.c
@@ -80,9 +80,11 @@ void printHashTable(Table T){
     options();
     for (i=0; i<T.buckets; i++) {
         if (strcmp(T.TableArray[i].key,EmptyKey)) {
-            printf("In pos %d is key %s\n",i,T.TableArray[i].key);
+        	if (T.TableArray[i].L == NULL)
+            	printf("In pos %d is key %s\n",i,T.TableArray[i].key);
             temp = T.TableArray[i].L;
-            printf("In position %d key(s): %s->",i,T.TableArray[i].key);
+            if (temp)
+            	printf("In position %d key(s): %s->",i,T.TableArray[i].key);
             while (temp) {
                 printf("%s->",temp->Entry.key);
                 temp = temp->next;
